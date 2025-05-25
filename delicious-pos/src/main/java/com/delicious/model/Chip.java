f59bfc644c15a180ed06a3ca5f4992957786bada
Amit Mainali <amitmainali4@gmail.com>
2025-05-24 20:22:02 -0400

    Add Drink and Chip classes implementing OrderItem

--- delicious-pos/src/main/java/com/delicious/model/Chip.java
+++ delicious-pos/src/main/java/com/delicious/model/Chip.java
@@ -1,4 +1,19 @@
 package com.delicious.model;
 
-public class Chip {
+public class Chip implements OrderItem {
+    private String type;
+
+    public Chip(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public double getPrice() {
+        return 1.50;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Chips - " + type;
+    }
 }
--- delicious-pos/src/main/java/com/delicious/model/Drink.java
+++ delicious-pos/src/main/java/com/delicious/model/Drink.java
@@ -1,4 +1,37 @@
 package com.delicious.model;
 
-public class Drink {
+import com.delicious.model.enums.Size;
+
+public class Drink implements OrderItem {
+    private Size size;
+    private String flavor;
+
+    public Drink(Size size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    @Override
+    public double getPrice() {
+        return switch (size) {
+            case SMALL -> 2.00;
+            case MEDIUM -> 2.50;
+            case LARGE -> 3.00;
+            default -> 0.00;
+        };
+    }
+
+    @Override
+    public String getDescription() {
+        return String.format("%s Drink - %s", formatSize(size), flavor);
+    }
+
+    private String formatSize(Size size) {
+        return switch (size) {
+            case SMALL -> "Small";
+            case MEDIUM -> "Medium";
+            case LARGE -> "Large";
+            default -> "";
+        };
+    }
 }
